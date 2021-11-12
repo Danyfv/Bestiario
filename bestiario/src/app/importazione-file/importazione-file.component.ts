eff698eca9964b6c5fe3d766cdcafdea56cd87ef
Daniele Fava <dany99f@gmail.com>
2021-11-12 20:52:08 +0100

    inizio lettura excel

--- bestiario/src/app/importazione-file/importazione-file.component.ts
+++ bestiario/src/app/importazione-file/importazione-file.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-importazione-file',
@@ -9,7 +10,36 @@ export class ImportazioneFileComponent implements OnInit {
 
   constructor() { }
 
+  arrayBuffer: any;
+  file: any;
+
   ngOnInit(): void {
   }
 
+  incomingfile(event: any)
+  {
+    console.log(`event`, event)
+    if(event &&  event.target && event.target.files){
+      this.file= event.target.files[0]; 
+    }
+  }
+  
+  Upload() {
+    debugger;
+        let fileReader = new FileReader();
+          fileReader.onload = (e) => {
+              this.arrayBuffer = fileReader.result;
+              var data = new Uint8Array(this.arrayBuffer);
+              var arr = new Array();
+              for(var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+              var bstr = arr.join("");
+              var workbook = XLSX.read(bstr, {type:"binary"});
+              var first_sheet_name = workbook.SheetNames[0];
+              var worksheet = workbook.Sheets[first_sheet_name];
+              console.log(XLSX.utils.sheet_to_json(worksheet,{raw:true}));
+          }
+          fileReader.readAsArrayBuffer(this.file);
+  }
+
+  
 }
